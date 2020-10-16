22dbf4fb8da01ade914c9232fe4833b749309a32
Jake Hannan <jhannan08@gmail.com>
2020-10-15 22:13:50 -0400

    add tourney gameplay!

---
+++ models/raw/tournament_gameplay.sql
@@ -0,0 +1,22 @@
+select
+  tournament_week_key
+  , game_title_key
+  , team_number
+  , athlete_user_key
+  , athlete
+  , match_id
+  , game_mode
+  , unix_match_start
+  , damage_done
+  , damage_taken
+  , deaths
+  , distance_traveled
+  , kill_death_ratio
+  , kills
+  , team_moving_pct as time_moving_pct
+  , score
+  , score_per_minute
+  , placement
+  , game_time_elapsed
+
+from {{ source('source', 'tournament_gameplay') }}
