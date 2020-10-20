e8b8b8c6310215b8b246c4fb62e90c8e3356c123
Jake Hannan <jhannan08@gmail.com>
2020-10-20 14:23:32 -0400

    fix quartile order to asc vs desc

--- models/marts/draft_data.sql
+++ models/marts/draft_data.sql
@@ -3,9 +3,11 @@ select
   , lp.tournament_week_key
   , lp.kd_ratio
   , row_number() over (partition by tournament_week_key order by kd_ratio desc) as kd_rank
-  , ntile(4) over (partition by tournament_week_key order by kd_ratio desc) as quartile
+  , ntile(4) over (partition by tournament_week_key order by kd_ratio asc) as quartile
   , lp.kills
   , lp.deaths
   , lp.wins
 
 from {{ ref('lifetime_gameplay') }} as lp
+
+order by tournament_week_key, kd_rank
